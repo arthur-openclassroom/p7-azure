d98e03e26ce4ef16e8550afe387c8654a32ef926
Arthur <arthur@trendex.tech>
2026-04-15 14:09:31 +0200

    fix: startup.sh bind on $PORT for azure

--- startup.sh
+++ startup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 pip install -r requirements.txt
 python -m nltk.downloader punkt punkt_tab stopwords wordnet
-gunicorn -w 2 -k uvicorn.workers.UvicornWorker api.main:app
+gunicorn -w 2 -k uvicorn.workers.UvicornWorker --bind "0.0.0.0:${PORT:-8000}" api.main:app
