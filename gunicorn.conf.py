644aa8f6929ccbdedd5a51515acaefa4b9aeac2b
Arthur <arthur@trendex.tech>
2026-04-15 13:59:42 +0200

    fix: use PORT env var for azure

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,9 +1,10 @@
+import os
 import multiprocessing
 
 max_requests = 1000
 max_requests_jitter = 50
 log_file = "-"
-bind = "0.0.0.0:8000"
+bind = f"0.0.0.0:{os.environ.get('PORT', '8000')}"
 timeout = 230
 num_cpus = multiprocessing.cpu_count()
 workers = (num_cpus * 2) + 1
